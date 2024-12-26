21cd81897eac5b8933a8386cf47de7a2b052d1f7
davidalba <david2001.004@gmail.com>
2024-12-26 04:48:30 -0500

    refact regex-grammar

--- src/cmp/regex_grammar.py
+++ src/cmp/regex_grammar.py
@@ -1,70 +1,50 @@
 from cmp.ast import *
 from cmp.ast_regex import *
 from cmp.pycompiler import Grammar
-# No Terminales
 
 
 G_Regex = Grammar("Regex")
 
-Expression = G_Regex.NonTerminal('Expression', True)  # No terminal inicial, marcado como inicial
-Alternative = G_Regex.NonTerminal('Alternative')
-StartAnchor = G_Regex.NonTerminal('StartAnchor')
-EndAnchor = G_Regex.NonTerminal('EndAnchor')
-Pattern = G_Regex.NonTerminal('Pattern')
-Subpattern = G_Regex.NonTerminal('Subpattern')
-Range = G_Regex.NonTerminal('Range')
-SymbolOrRange = G_Regex.NonTerminal('SymbolOrRange')
-Number = G_Regex.NonTerminal('NUMBER')
-MoreSymbols = G_Regex.NonTerminal('MoreSymbols')
+# No Terminales
+expression = G_Regex.NonTerminal('EXPRESSION', True)  # No terminal inicial, marcado como inicial
+alternative, start_anchor, end_anchor, pattern, subpattern, in_range, symbol_or_range, number, more_symbols = G_Regex.NonTerminals(
+    'ALTERNATIVE STARTANCHOR ENDANCHOR PATTERN SUBPATTERN INRANGE SYMBOLORRANGE NUMBER MORESYMBOLS')
+
+
 # Terminales con etiquetas específicas
-question = G_Regex.Terminal('QUESTION')
-dot = G_Regex.Terminal('DOT')
-plus = G_Regex.Terminal('PLUS')
-pipe = G_Regex.Terminal('PIPE')
-star = G_Regex.Terminal('STAR')
-minus = G_Regex.Terminal('MINUS')
-caret = G_Regex.Terminal('CARET')
-opar = G_Regex.Terminal('OPAR')
-cpar = G_Regex.Terminal('CPAR')
-obrckt  = G_Regex.Terminal('OBRCKT')
-cbrckt  = G_Regex.Terminal('CBRCKT')
-symbol  = G_Regex.Terminal('SYMBOL')
-
-epsilon = G_Regex.Terminal('EPSILON') 
-dollar = G_Regex.Terminal('DOLLAR')   
-obrace = G_Regex.Terminal('OBRACE')   
-cbrace = G_Regex.Terminal('CBRACE')
-
-Expression %= Expression + pipe + Alternative, lambda h,s: UnionNode(s[1],s[3])
-Expression %= Alternative , lambda h,s: s[1]
-
-Alternative %= caret + EndAnchor ,lambda h,s: StarAnchorNode(s[2])
-EndAnchor %= Pattern + dollar ,lambda h,s: EndAnchorNode(s[1])
-EndAnchor %= Pattern,lambda h,s: s[1]
-Alternative %= EndAnchor ,lambda h,s: s[1]
+question, dot, plus, pipe, star, minus, caret, opar, cpar, obrckt, cbrckt, epsilon, dollar, obrace, cbrace, symbol = G_Regex.Terminals(
+    'QUESTION DOT PLUS PIPE STAR MINUS CARET OPAR CPAR OBRCKT CBRCKT EPSILON DOLLAR OBRACE CBRACE SYMBOL')
 
 
-Pattern  %= Pattern + Subpattern ,lambda h,s: ConcatNode(s[1],s[2])
-Pattern  %= Pattern+ dot + Subpattern ,lambda h,s: DotNode(s[1],s[3])
-Pattern  %= Subpattern ,lambda h,s: s[1]
+expression %= expression + pipe + alternative, lambda h, s: UnionNode(s[1], s[3])
+expression %= alternative, lambda h, s: s[1]
 
-Subpattern %= Subpattern + star ,lambda h,s: ClosureNode(s[1])
-Subpattern %= Subpattern + plus ,lambda h,s: PlusNode(s[1])
-Subpattern %= Subpattern + question ,lambda h,s: QuestNode(s[1])
-Subpattern %= Subpattern + obrace + Number +cbrace ,lambda h,s: QuantifierNode(s[2],s[2])
-Subpattern %= Subpattern + obrace + Number + minus + Number +cbrace ,lambda h,s:QuantifierNode(s[1],s[3],s[5])
-Number %= MoreSymbols, lambda h,s: NumberNode(s[1])
-MoreSymbols %= MoreSymbols + symbol, lambda h,s : s[1]+s[2]
-MoreSymbols %= symbol,lambda h,s : s[1]
-Subpattern %= obrckt + Range + cbrckt ,lambda h,s: RangeNode(s[2][0],s[2][1])
-Subpattern %= obrckt + caret + Range + cbrckt, lambda h,s: NotRangeNode(s[3][0],s[3][1]) 
-Subpattern %= opar + Expression + cpar ,lambda h,s: s[2]
-Subpattern %= symbol ,lambda h,s:  SymbolNode(s[1])
-Subpattern %= epsilon ,lambda h,s: EpsilonNode(s[1])
+alternative %= caret + end_anchor, lambda h, s: StarAnchorNode(s[2])
+end_anchor %= pattern + dollar, lambda h, s: EndAnchorNode(s[1])
+end_anchor %= pattern, lambda h, s: s[1]
+alternative %= end_anchor, lambda h, s: s[1]
 
-Range %= Range + SymbolOrRange ,lambda h,s: list(map(lambda t: t[0] + t[1], zip(s[1],s[2])))
-Range %= SymbolOrRange ,lambda h,s: s[1]
 
-SymbolOrRange %= symbol + minus +symbol ,lambda h,s: ([],[(s[1],s[3])])
-SymbolOrRange %= symbol ,lambda h,s: ([s[1]],[])
+pattern %= pattern + subpattern, lambda h, s: ConcatNode(s[1], s[2])
+pattern %= pattern + dot + subpattern, lambda h, s: DotNode(s[1], s[3])
+pattern %= subpattern, lambda h, s: s[1]
 
+subpattern %= subpattern + star, lambda h, s: ClosureNode(s[1])
+subpattern %= subpattern + plus, lambda h, s: PlusNode(s[1])
+subpattern %= subpattern + question, lambda h, s: QuestNode(s[1])
+subpattern %= subpattern + obrace + number + cbrace, lambda h, s: QuantifierNode(s[2], s[2])
+subpattern %= subpattern + obrace + number + minus + number + cbrace, lambda h, s: QuantifierNode(s[1], s[3], s[5])
+number %= more_symbols, lambda h, s: NumberNode(s[1])
+more_symbols %= more_symbols + symbol, lambda h, s: s[1] + s[2]
+more_symbols %= symbol, lambda h, s: s[1]
+subpattern %= obrckt + in_range + cbrckt, lambda h, s: RangeNode(s[2][0], s[2][1])
+subpattern %= obrckt + caret + in_range + cbrckt, lambda h, s: NotRangeNode(s[3][0], s[3][1])
+subpattern %= opar + expression + cpar, lambda h, s: s[2]
+subpattern %= symbol, lambda h, s: SymbolNode(s[1])
+subpattern %= epsilon, lambda h, s: EpsilonNode(s[1])
+
+in_range %= in_range + symbol_or_range, lambda h, s: list(map(lambda t: t[0] + t[1], zip(s[1], s[2])))
+in_range %= symbol_or_range, lambda h, s: s[1]
+
+symbol_or_range %= symbol + minus + symbol, lambda h, s: ([], [(s[1], s[3])])
+symbol_or_range %= symbol, lambda h, s: ([s[1]], [])
